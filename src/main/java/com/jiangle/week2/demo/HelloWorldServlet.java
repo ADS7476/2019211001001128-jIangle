b1590c85f53af5cd61cf265677bd0b52597913c7
2019211001001128-jiangle <747618516@qq.com>
2021-03-18 19:55:50 +0800

    HW-Week2 Commit

---
+++ src/main/java/com/jiangle/week2/demo/HelloWorldServlet.java
@@ -0,0 +1,29 @@
+package com.jiangle.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//now its just a java class
+//extend HttpServlet
+public class HelloWorldServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws  IOException {
+
+//when client request method is GET - here - inside doGet()
+//we want to send HeLlo to client
+//we need to write HeLlo in response
+        //get writer - java .io
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello Client !!!");//that all
+        //next we need to tell about this servLet to tomcat - how ? - web.xmL
+
+
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response){
+//when client request method is Post - here - inside doPost()
+    }
+}
