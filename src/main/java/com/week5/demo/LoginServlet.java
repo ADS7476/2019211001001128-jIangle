9da8f4a45a4a0088594725cfb93412b8266d40f2
2019211001001128-jiangle <747618516@qq.com>
2021-04-08 17:19:22 +0800

    HW-Week5 Commit

---
+++ src/main/java/com/week5/demo/LoginServlet.java
@@ -0,0 +1,80 @@
+package com.week5.demo;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet(name = "LoginServlet",value = "/login")
+public class LoginServlet extends HttpServlet {
+    Connection con=null; //class variable
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        //only one connection
+        //String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        //String url="jdbc:sqlserver://localhost:1433;DatabaseName=userdb";
+        //String username="sa";
+        //String password="123456";
+        //code like this is bad way --because change in not easy
+        //for example change password of db = change in java code
+
+        //get servletconfig --> getInitParameters --no change
+        String driver= getServletContext().getInitParameter("driver");
+        String url= getServletContext().getInitParameter("url");
+        String username= getServletContext().getInitParameter("username");
+        String password= getServletContext().getInitParameter("password");
+
+
+        try {
+            Class.forName(driver);
+            con= DriverManager.getConnection(url,username,password);
+            System.out.println("init()-->"+con); //ok(use java code) -ok (use config in web.xml) -ok --use(@webservlet)
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        PrintWriter writer = response.getWriter();
+
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        String sql_1="select * from [user]"+
+                "WHERE username='"+username+"'"+" AND "+
+                "password = '"+password+"'"+
+                ';';
+        ResultSet rs= null;
+        try {
+            rs = con.createStatement().executeQuery(sql_1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try {
+            if(!rs.next()){
+                writer.println("Login Success!!!");
+                writer.println("Welcome ChenLiang");
+
+            }
+            else
+            {
+                writer.println("密码或用户名错误");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
